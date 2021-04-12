7dce1f9c7c1de4d61f11bfaf1c2a2372dd9e3e53
sherry wu <sherrywu@sherrys-MBP.hsd1.ca.comcast.net>
2021-04-11 20:54:17 -0700

    navbar build & connect auth0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import { Button } from "@material-ui/core";
 import { Route, Switch, useHistory } from "react-router-dom";
 import Example from "src/pages/example";
+import Profile from "./pages/Profile";
+import NavbarComponent from "./pages/NavbarComponent";
+import "bootstrap/dist/css/bootstrap.css";
 
 const App = () => {
   const history = useHistory();
 
   return (
     <div>
+      <NavbarComponent/>
       <Switch>
+        <Route path="/profile/:userId" exact component={Profile}/>
         <Route exact path='/example'>
           <Example />
         </Route>
