cd9962f8090b11803ebb3680bf86ecc2e57f591b
YeqingHuang <yeqingh22@gmail.com>
2021-04-19 10:52:30 -0700

    add getUserMetadate function in the backend

--- backend/src/routers/example.ts
+++ backend/src/routers/example.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import getUserMetadate from "../utils/userUtils";
 
 export const exampleRouter = express.Router();
 
 exampleRouter.get("/", (request, response) => {
+  getUserMetadate("auth0|607cd3d1599f83006ad55079").then((result: any) => {
+    console.log(result);
+  });
+
   response.send({ message: "example api" });
 });
