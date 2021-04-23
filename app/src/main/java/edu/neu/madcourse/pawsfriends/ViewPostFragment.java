13aee41352b6b1cde0dc65eaf359047d9e7524a4
yiqiandeng <dengyiqian1993@gmail.com>
2021-04-23 17:27:55 -0400

    GridImage Selection Interface

---
+++ app/src/main/java/edu/neu/madcourse/pawsfriends/ViewPostFragment.java
@@ -0,0 +1,22 @@
+package edu.neu.madcourse.pawsfriends;
+
+import android.os.Bundle;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ViewPostFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view_post, container, false);
+
+        return view;
+    }
+}
